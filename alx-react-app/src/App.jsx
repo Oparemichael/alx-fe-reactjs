8444825d6629598a2e42789f2155f36949b55151
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-07-13 19:17:45 +0000

    Update App.jsx import UserProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage.jsx'
 import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
+import UserProfile './components/UserProfile.jsx'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -17,6 +18,7 @@ function App() {
         <Header />
         <MainContent />
         <Footer />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
