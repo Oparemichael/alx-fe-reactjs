44152c82762c68118fecbac33b30c03359ffea00
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-07-13 18:41:12 +0000

    Update App.jsx import header, Maincontent and Footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage.jsx'
+import Header from './components/Header.jsx'
+import MainContent from './components/MainContent.jsx'
+import Footer from './components/Footer.jsx'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -11,6 +14,9 @@ function App() {
     <>
       <div>
         <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
