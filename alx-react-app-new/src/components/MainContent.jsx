0ad4d52c370c9b015a27ae531d792bfcd02d0b49
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-07-19 06:20:37 +0000

    fix: add styling to paragraph in MainContent component

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
        return(
  <main>
-  <p>I love to visit New York, Paris, and Tokyo.</p>
+  <p style={{ color:'grey', fontWeight:'bold'}}>I love to visit New York, Paris, and Tokyo.</p>
 </main>
 
 );
