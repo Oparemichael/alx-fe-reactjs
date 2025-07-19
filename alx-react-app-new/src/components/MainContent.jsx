c0477846b57af5073be5cf6c160c8e3758ddddb4
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-07-19 06:24:26 +0000

    fix: update styling for MainContent component's main tag

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
        return(
- <main style={{ color:'grey', fontWeight:'bold'}}>
+ <main style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center'}}>
   <p>I love to visit New York, Paris, and Tokyo.</p>
 </main>
 
