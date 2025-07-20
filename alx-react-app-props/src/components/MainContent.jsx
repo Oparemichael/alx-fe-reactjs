f43eefd275891f812c9b659bed5fbde612809709
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-07-20 21:16:30 +0000

    feat: Initialize React app with Vite, add core components and styles
    - Created package.json with dependencies for React, Vite, and ESLint.
    - Added Vite logo SVG and React logo SVG to public assets.
    - Implemented main application structure in App.jsx, including WelcomeMessage, Header, MainContent, Footer, and UserProfile components.
    - Developed individual components: WelcomeMessage, Header, MainContent, Footer, and UserProfile with basic content.
    - Styled application with App.css and index.css for layout and design.
    - Configured Vite for development and build processes in vite.config.js.

---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+       return(
+ <main style={{ color:'grey', fontWeight:'bold'}}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+
+);
+};
+
+export default MainContent;
