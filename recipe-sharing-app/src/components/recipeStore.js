ab82131e8029cb4365f4d9d89c593f3ef1d695db
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-07-25 12:42:29 +0000

    fix: change export syntax for useRecipeStore in recipeStore component

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,4 +5,4 @@ const useRecipeStore = create((set) => ({
   addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes })
 }));
-export { useRecipeStore };
+export default { useRecipeStore };
