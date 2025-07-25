bf15ac252ce20c1967942a0cb2085aa542fbf04d
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-07-25 12:33:41 +0000

    fix: correct import syntax for Zustand in recipeStore component

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,6 +1,6 @@
-import create from 'zustand'
+import { create } from 'zustand'
 
-const useRecipeStore = create(set => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes })
