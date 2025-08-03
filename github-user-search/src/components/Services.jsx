1f841426f7a5ef5aa1e1fc9f5e9f66da62a3b6ca
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-08-03 23:58:53 +0000

    Recreate Services.jsx

---
+++ github-user-search/src/components/Services.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { fetchUserData } from '../services/githubService';
+import '../index.css';
+
+const Search = () => {
+    const [username, setUsername] = useState('');
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const handleInputChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setLoading(true);
+        setError(null);
+        setUserData(null);
+
+        try {
+            const data = await fetchUserData(username);
+            setUserData(data);
+        } catch (error) {
+            setError('Looks like we can\'t find the user');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div className="form-container">
+            <form onSubmit={handleSubmit} className="form">
+                <input
+                    type="text"
+                    value={username}
+                    onChange={handleInputChange}
+                    placeholder="Enter GitHub username"
+                    className="input-field"
+                />
+                <button type="submit" className="submit-button">Search</button>
+            </form>
+
+            {loading && <p className="loading-message">Loading...</p>}
+            {error && <p className="error-message">{error}</p>}
+            {userData && (
+                <div className="user-data-container">
+                    <img src={userData.avatar_url} alt={`${userData.login}'s avatar`} className="user-avatar" />
+                    <h2 className="user-name">{userData.name}</h2>
+                    <p>
+                        <a href={userData.html_url} target="_blank" rel="noopener noreferrer" className="profile-link">
+                            View GitHub Profile
+                        </a>
+                    </p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Search;
