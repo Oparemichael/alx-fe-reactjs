feb8fe2959c003e0874035454091755d24d1e1e0
Michael Opare Gyeketey <oparemic1@gmail.com>
2025-08-06 21:18:53 +0000

    completed search componenet

--- github-user-search/src/assets/components/Search.jsx
+++ github-user-search/src/assets/components/Search.jsx
@@ -9,6 +9,34 @@ const GitHubUserSearch = () => {
         e.preventDefault();
         setError('');
         setUserData(null);
+         
+        try {
+            const data = await fetchUserData(username);
+            setUserData(data);
+        }
+        catch (err) {
+            setError('Looks like we cant find the user');
+        }
+        finally {
+            setLoading(false);
+        }
 
-    }
-}
+    };
+
+    return (
+        <div>
+            < form onSubmit = {handleSubmit}>
+            <input
+            type="text"
+            placeholder="Enter Github username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            />
+            <button type="submit">Search</button>
+        </form>
+        </div>
+    )
+};
+
+export default GitHubUserSearch;
