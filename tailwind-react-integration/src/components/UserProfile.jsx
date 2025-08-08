f3fdb6b6b77caf06463403e4259da7dddafb13ed
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-08-08 17:54:26 +0000

    Updates

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 md:max-w-sm max-w-xs sm:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 md:max-w-sm max-w-xs sm:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img 
       src="https://via.placeholder.com/150"
       alt="User"
-      classname="rounded-full w-36 h-36 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto mb-3 sm:mb-4 "
+      classname="rounded-full w-36 h-36 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto mb-3 sm:mb-4 hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 classname="text-lg md:text-xl sm:text-x1 md:text-2x1 font-bold sm:mb-2 text-blue-800 my-4 text-center">John Doe</h1>
+      <h1 classname="text-lg md:text-xl sm:text-x1 md:text-2x1 font-bold sm:mb-2 text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300 ease-in-out">John Doe</h1>
       <p clasname="text-gray-600 text-base text-center text-sm sm:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
