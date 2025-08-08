d1109f485e588e5595c6291eae6a55f3c3e601c5
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-08-08 17:31:41 +0000

    Update Userprofile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -8,7 +8,7 @@ function UserProfile() {
       alt="User"
       classname="rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 mx-auto mb-3 sm:mb-4 "
       />
-      <h1 classname="text-lg sm:text-x1 md:text-2x1 font-bold sm:mb-2 text-blue-800 my-4 text-center">John Doe</h1>
+      <h1 classname="text-lg md:text-xl sm:text-x1 md:text-2x1 font-bold sm:mb-2 text-blue-800 my-4 text-center">John Doe</h1>
       <p clasname="text-gray-600 text-base text-center text-sm sm:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
