fb92fe3ab8ba1e0c31c028925fdc48e0b4442894
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-08-08 17:34:21 +0000

    Update

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
       <img 
       src="https://via.placeholder.com/150"
       alt="User"
-      classname="rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 mx-auto mb-3 sm:mb-4 "
+      classname="rounded-full w-36 h-36 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto mb-3 sm:mb-4 "
       />
       <h1 classname="text-lg md:text-xl sm:text-x1 md:text-2x1 font-bold sm:mb-2 text-blue-800 my-4 text-center">John Doe</h1>
       <p clasname="text-gray-600 text-base text-center text-sm sm:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
