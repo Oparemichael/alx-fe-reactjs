6166cda18c0dfc3901a3d09b5d40cb713de9499a
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-08-08 17:00:51 +0000

    image with and height was adjusted in the Userprofile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
       <img 
       src="https://via.placeholder.com/150"
       alt="User"
-      classname="rounded-full w-32 h-32 mx-auto mb-4"
+      classname="rounded-full w-36 h-36 mx-auto mb-4"
       />
       <h1 classname="text-x1 text-blue-800 my-4 text-center">John Doe</h1>
       <p clasname="text-gray-600 text-base text-center">Developer at Example Co. Loves to write code and explore new technologies.</p>
