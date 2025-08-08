b17da4b06fee89f6d77cb4c2822803f0debf4cc8
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-08-08 16:55:49 +0000

    Correct errors in UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,11 @@ import React from "react";
 function UserProfile() {
   return (
     <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      https://via.placeholder.com/150"
+      <img 
+      src="https://via.placeholder.com/150"
+      alt="User"
+      classname="rounded-full w-32 h-32 mx-auto mb-4"
+      />
       <h1 classname="text-x1 text-blue-800 my-4 text-center">John Doe</h1>
       <p clasname="text-gray-600 text-base text-center">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
