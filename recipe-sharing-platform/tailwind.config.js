a528aaee0d4e939ce5c5d4b180658fc64296b692
Michael Opare Gyeketey <oparemic1@gmail.com>
2025-08-13 23:09:50 +0000

    updates on configuration

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // or 'media' or false
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
