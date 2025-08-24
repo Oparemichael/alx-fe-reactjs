05b88685fe02920f7edc260e2e4d1714852fde9a
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-08-24 23:10:11 +0000

    Directories update

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
-import PostsComponent from './components/PostsComponent'
+import PostsComponent from './PostsComponent'
 import './App.css'
 
 // Create a client
