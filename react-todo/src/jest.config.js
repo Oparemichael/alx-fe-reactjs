b8f2da12f6d33bab6b190c55ebc299aba5ec87e4
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-08-26 12:34:07 +0000

    Updated Test configurations

---
+++ react-todo/src/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapping: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+  },
+};
