6b86853ec8c659818a5df3e52351a4e9c34a7e80
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-08-26 12:22:43 +0000

    Added testing

--- react-todo/src/__tests__/AddTodoForm.test.js
+++ react-todo/src/__tests__/AddTodoForm.test.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import AddTodoForm from '../components/AddTodoForm';
 
 describe('AddTodoForm Component', () => {
--- react-todo/src/__tests__/TodoItem.js
+++ react-todo/src/__tests__/TodoItem.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import TodoItem from '../components/TodoItem';
 
 describe('TodoItem Component', () => {
