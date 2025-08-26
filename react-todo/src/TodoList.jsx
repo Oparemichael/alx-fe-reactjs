10f8c475b7368b74900bee994c1c20bd5f11546e
Michael Opare Gyeketey <164248767+Oparemichael@users.noreply.github.com>
2025-08-26 11:45:40 +0000

    Added test files

---
+++ react-todo/src/TodoList.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import AddTodoForm from './AddTodoForm';
+import TodoItem from './TodoItem';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', completed: false },
+    { id: 2, text: 'Build a Todo App', completed: true },
+    { id: 3, text: 'Write Tests', completed: false }
+  ]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text,
+      completed: false
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <div className="todo-list">
+      <h1>Todo List</h1>
+      <AddTodoForm onAdd={addTodo} />
+      <ul>
+        {todos.map(todo => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onToggle={toggleTodo}
+            onDelete={deleteTodo}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
